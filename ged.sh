57527eea5f1898746735887017887c1f54811ac8
Relintai <relintai@protonmail.com>
2023-01-13 10:47:35 +0100

    Update the quick open script.

--- ged.sh
+++ ged.sh
@@ -3,4 +3,4 @@
 cp -u ./pandemonium_engine/bin/pandemonium.x11.opt.tools.64 ./pandemonium_engine/bin/run.pandemonium.x11.opt.tools.64
 
 export LD_LIBRARY_PATH=`pwd`/pandemonium_engine/bin/
-./pandemonium_engine/bin/run.pandemonium.x11.opt.tools.64 -e --path ./game/
+./pandemonium_engine/bin/run.pandemonium.x11.opt.tools.64 -e --path ./godot/
