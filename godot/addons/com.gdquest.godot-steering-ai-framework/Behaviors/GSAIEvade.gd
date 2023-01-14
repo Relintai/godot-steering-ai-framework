0c863b099d03e19834d705b5c1d76de5ab7243af
Relintai <relintai@protonmail.com>
2023-01-14 10:09:48 +0100

    Updated the engine and removed every class that is now in c++.

--- godot/addons/com.gdquest.godot-steering-ai-framework/Behaviors/GSAIEvade.gd
+++
@@ -1,9 +0,0 @@
-class_name GSAIEvade
-extends GSAIPursue
-
-# Calculates acceleration to take an agent away from where a target agent is
-# moving.
-# @category - Individual behaviors
-
-func _get_modified_acceleration() -> float:
-	return -agent.linear_acceleration_max
--- godot/addons/com.gdquest.godot-steering-ai-framework/Behaviors/GSAIFlee.gd
+++
@@ -1,9 +0,0 @@
-class_name GSAIFlee
-extends GSAISeek
-
-# Calculates acceleration to take an agent directly away from a target agent.
-# @category - Individual behaviors
-
-func _calculate_steering(acceleration: GSAITargetAcceleration) -> void:
-	acceleration.linear = ((agent.position - target.position).normalized() * agent.linear_acceleration_max)
-	acceleration.angular = 0
